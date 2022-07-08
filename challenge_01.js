e9941c4b33fc273956150c857571f53a7e919206
BaeckerAnna <baecker.anna2@gmail.com>
2022-07-08 12:06:25 +0200

    Drei Rechenmethoden zum addieren

--- challenge_01.js
+++ challenge_01.js
@@ -9,3 +9,38 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
 
+
+
+/******* Methode 1 *******/
+
+// let zahl1, zahl2;
+// zahl1 = prompt("Bitte eine Zahl eingeben: ");
+// zahl2 = prompt("Bitte noch eine Zahl eingeben: ");
+
+// summe = parseInt(zahl1) + parseInt(zahl2);               // 1. Rechenmethode
+
+// console.log(summe);
+
+
+
+/******* Methode 2 *******/
+
+// let zahl1, zahl2;
+// zahl1 = prompt("Bitte eine Zahl eingeben: ");
+// zahl2 = prompt("Bitte noch eine Zahl eingeben: ");
+
+// summe = zahl1*1 + zahl2*1;                              // 2. Rechenmethode
+
+// console.log(summe);
+
+
+
+/******* Methode 3 *******/
+
+let zahl1, zahl2;
+zahl1 = prompt("Bitte eine Zahl eingeben: ");
+zahl2 = prompt("Bitte noch eine Zahl eingeben: ");
+
+summe = zahl1 - - zahl2;                                    // 3. Rechenmethode
+
+console.log(summe);
